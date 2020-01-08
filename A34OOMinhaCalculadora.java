611503d0ce54f44bcaaa67c23f6d1791052567a8
Josius <josius.amaro@gmail.com>
2020-01-08 15:41:39 -0300

    aula 34

--- A34OOMinhaCalculadora.java
+++ A34OOMinhaCalculadora.java
@@ -64,23 +64,25 @@ public class A33OOTesteCalculadora{
 
 public class A34OOMinhaCalculadora{
 	
-	public int soma(int num1, int num2){
+	public static int soma(int num1, int num2){
 		return num1+num2;
 	}
 	
-	public double soma(double num1, double num2){
+	public static double soma(double num1, double num2){
 		return num1+num2;
 	}
 	
-	public int soma(int num1, int num2, int num3){
+	public static int soma(int num1, int num2, int num3){
 		return num1+num2+num3;
 	}
 	
-	public int soma(int[] vetorInteiros){
+	//VERIFICAR PQ O CÓDIGO ABAIXO NÃO FUNCIONA
+	/*
+	public static int soma(int[] vetorInteiros){
 		int total = 0;
 		for(int i=0; i<vetorInteiros; i++){
 			total += vetorInteiros[i];
 		}
 		return total;
-	}
+	}*/
 }
--- A34OOTesteCalculadora.java
+++ A34OOTesteCalculadora.java
@@ -1,5 +1,31 @@
 public class A34OOTesteCalculadora{
+	//Para que o metodo main acesse a variavel abaixo, faz-se necessario o uso da palavra reservada 'static', do contrario, ela nao sera acessivel.
+	static int resultSoma;
+	
 	public static void main(String[] args){
 		
+		//acessando a variavel resultSoma
+		resultSoma = A34OOMinhaCalculadora.soma(5, 6);
+		
+		//A34OOMinhaCalculadora calc = new A34OOMinhaCalculadora();
+		
+		//calc.soma(1, 2);
+		
+		//Usando a classe 'A34OOMinhaCalculadora' sem instanciar o objeto
+		A34OOMinhaCalculadora.soma(1, 2);
+		
+		//acessando o metodo soma2Valores
+		
+		int s2v = soma2Valores(3, 4);
+		soma2Valores(7, 8);
+		System.out.println(resultSoma);
+		//System.out.println(soma); não funciona
+		System.out.println(s2v);
+		//System.out.println(soma2Valores); não funciona
+	}
+	
+	//Declarando um metodo que sera acessado somente pelo main
+	static int soma2Valores(int num1, int num2){
+		return A34OOMinhaCalculadora.soma(num1, num2);
 	}
 }
