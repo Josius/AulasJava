c81c80755252444a75be07ff62e6c632793fdbe4
Josius <josius.amaro@gmail.com>
2020-02-19 14:34:01 -0300

    aula60

---
+++ Aula 60 - Escopo de Variáveis/Teste.java
@@ -0,0 +1,25 @@
+public class Teste{
+	
+	public static void main(String[] args){
+		
+		EscopoVariaveis escopo = new EscopoVariaveis();
+		escopo.setValor(10);
+			
+		System.out.println(escopo.getValor());
+		
+		System.out.println(escopo.calculaValor(20));
+		
+		System.out.println(escopo.getValor());
+				
+		System.out.println(escopo.teste());		
+		
+		System.out.println(escopo.getValor());
+		
+		escopo.outroTeste();
+		
+		escopo.maisUmTeste();
+		
+		escopo.maisUmOutroTeste(10);
+		
+	}
+}
